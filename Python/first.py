aeca131f6a9c0cc3fff3c77fde8444acd76f1407
DohuiKo <ksder99@naver.com>
2021-05-16 16:57:34 +0900

    message

---
+++ Python/first.py
@@ -0,0 +1,17 @@
+print('멋쟁이사자처럼')
+print(1+2)
+
+banjang = "강민철"
+
+print("우리반 반장 이름은", banjang)
+
+banjang2 = input("반장의 이름을 입력하세요.")
+
+print("우리반 반장 이름은",banjang2,"입니다.")
+
+number1 = int(input)("첫번째 문자를 입력하세요. :")
+number2 = int(input)("두번째 문자를 입력하세요. :")
+
+print(number1+number2)
+
+
